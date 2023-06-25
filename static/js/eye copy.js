9fbb74e429c1e230675e34485f7fde29c175a58a
Matthew <matthew_holmes@Matthews-MacBook-Pro.local>
2023-06-24 23:11:20 -0700

    added updates

---
+++ static/js/eye copy.js
@@ -0,0 +1,183 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var eyeContainer = document.getElementById("eye-container");
+  var leftEye = document.getElementById("left-eye");
+  var rightEye = document.getElementById("right-eye");
+  var leftPupil = leftEye.querySelector(".pupil");
+  var rightPupil = rightEye.querySelector(".pupil");
+  var face = document.getElementById("face");
+  // initialize with black screen
+  leftEye.style.backgroundColor = "black";
+  rightEye.style.backgroundColor = "black";
+
+  var eyeMultiplier = 0.09; // Adjust this value for eye movement
+  var pupilMultiplier = 0.08; // Adjust this value for pupil movement
+  var judgment = document.getElementById("judgment");
+  var judge = true;
+  function easeOutCubic(t) {
+    return 1 - Math.pow(1 - t, 3);
+  }
+  function updateEyePositions() {
+    var faceRect = face.getBoundingClientRect();
+    var faceCenterX = faceRect.left + faceRect.width / 2;
+    var faceCenterY = faceRect.top + faceRect.height / 2;
+
+    var containerRect = eyeContainer.getBoundingClientRect();
+    var containerCenterX = containerRect.left + containerRect.width / 2;
+    var containerCenterY = containerRect.top + containerRect.height / 2;
+
+    var maxEyeDistance = Math.min(
+      containerRect.width / 4,
+      containerRect.height / 4
+    );
+    var maxPupilDistance = maxEyeDistance * pupilMultiplier;
+
+    var dx = containerCenterX - faceCenterX;
+    var dy = containerCenterY - faceCenterY;
+
+    var eyeTranslationX = dx * eyeMultiplier;
+    var eyeTranslationY = dy * eyeMultiplier;
+
+    var pupilTranslationX = (dx / containerRect.width) * maxPupilDistance;
+    var pupilTranslationY = (dy / containerRect.height) * maxPupilDistance;
+
+    var maxEyeTranslationX = maxEyeDistance * eyeMultiplier;
+    var maxEyeTranslationY = maxEyeDistance * eyeMultiplier;
+    var maxPupilTranslationX =
+      (leftEye.offsetWidth - leftPupil.offsetWidth) / 2;
+    var maxPupilTranslationY =
+      (leftEye.offsetHeight - leftPupil.offsetHeight) / 2;
+
+    eyeTranslationX = Math.max(
+      -maxEyeTranslationX,
+      Math.min(maxEyeTranslationX, eyeTranslationX)
+    );
+    eyeTranslationY = Math.max(
+      -maxEyeTranslationY,
+      Math.min(maxEyeTranslationY, eyeTranslationY)
+    );
+
+    pupilTranslationX = Math.max(
+      -maxPupilTranslationX,
+      Math.min(maxPupilTranslationX, pupilTranslationX)
+    );
+    pupilTranslationY = Math.max(
+      -maxPupilTranslationY,
+      Math.min(maxPupilTranslationY, pupilTranslationY)
+    );
+
+    var eyeTransform = `translate(${eyeTranslationX}px, ${eyeTranslationY}px)`;
+    var pupilTransform = `translate(${pupilTranslationX}px, ${pupilTranslationY}px)`;
+
+    leftEye.style.transform = eyeTransform;
+    rightEye.style.transform = eyeTransform;
+
+    leftPupil.style.transform = pupilTransform;
+    rightPupil.style.transform = pupilTransform;
+
+    // Center the eyes within the face
+    var eyeContainerRect = eyeContainer.getBoundingClientRect();
+    var faceWidth = faceRect.width;
+    var faceHeight = faceRect.height;
+    var eyeContainerWidth = eyeContainerRect.width;
+
+    var offsetLeft = (faceWidth - eyeContainerWidth) / 2;
+    eyeContainer.style.left = offsetLeft + "px";
+  }
+
+  function handleFaceVisibility() {
+    if (judge) {
+      leftEye.style.display = "none";
+      leftPupil.style.display = "none";
+      rightEye.style.display = "none";
+      rightPupil.style.display = "none";
+      face.style.display = "none";
+    } else {
+      leftEye.style.display = "block";
+      leftPupil.style.display = "block";
+      rightEye.style.display = "block";
+      rightPupil.style.display = "block";
+      face.style.display = "block";
+
+      // Call updateEyePositions to center the eyes within the face
+      setTimeout(updateEyePositions, 0);
+    }
+  }
+
+  function toggleJudgment() {
+    judge = !judge;
+    handleFaceVisibility();
+    if (judge) {
+      leftEye.style.animation = "hideEyes 2s forwards";
+      rightEye.style.animation = "hideEyes 2s forwards";
+    } else {
+      // Show eyes with animation
+      leftEye.style.animation = "revealEyes 2s backwards";
+      rightEye.style.animation = "revealEyes 2s backwards";
+    }
+  }
+  judgment.addEventListener("click", toggleJudgment);
+  window.addEventListener("resize", function () {
+    updateEyePositions();
+  });
+
+  document.addEventListener("mousemove", function (event) {
+    var containerRect = eyeContainer.getBoundingClientRect();
+    var containerCenterX = containerRect.left + containerRect.width / 2;
+    var containerCenterY = containerRect.top + containerRect.height / 2;
+
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+
+    var dx = mouseX - containerCenterX;
+    var dy = mouseY - containerCenterY;
+
+    var maxEyeDistance = Math.min(
+      containerRect.width / 4,
+      containerRect.height / 4
+    );
+    var maxPupilDistance = maxEyeDistance * pupilMultiplier;
+
+    var eyeTranslationX = dx * eyeMultiplier;
+    var eyeTranslationY = dy * eyeMultiplier;
+
+    var pupilTranslationX = (dx / containerRect.width) * maxPupilDistance;
+    var pupilTranslationY = (dy / containerRect.height) * maxPupilDistance;
+
+    var maxEyeTranslationX = maxEyeDistance * eyeMultiplier;
+    var maxEyeTranslationY = maxEyeDistance * eyeMultiplier;
+    var maxPupilTranslationX =
+      (leftEye.offsetWidth - leftPupil.offsetWidth) / 2;
+    var maxPupilTranslationY =
+      (leftEye.offsetHeight - leftPupil.offsetHeight) / 2;
+
+    eyeTranslationX = Math.max(
+      -maxEyeTranslationX,
+      Math.min(maxEyeTranslationX, eyeTranslationX)
+    );
+    eyeTranslationY = Math.max(
+      -maxEyeTranslationY,
+      Math.min(maxEyeTranslationY, eyeTranslationY)
+    );
+
+    pupilTranslationX = Math.max(
+      -maxPupilTranslationX,
+      Math.min(maxPupilTranslationX, pupilTranslationX)
+    );
+    pupilTranslationY = Math.max(
+      -maxPupilTranslationY,
+      Math.min(maxPupilTranslationY, pupilTranslationY)
+    );
+
+    var eyeTransform = `translate(${eyeTranslationX}px, ${eyeTranslationY}px)`;
+    var pupilTransform = `translate(${pupilTranslationX}px, ${pupilTranslationY}px)`;
+
+    leftEye.style.transform = eyeTransform;
+    rightEye.style.transform = eyeTransform;
+
+    leftPupil.style.transform = pupilTransform;
+    rightPupil.style.transform = pupilTransform;
+  });
+
+  handleFaceVisibility();
+  updateEyePositions();
+});
---
+++ static/js/scroll.js
@@ -0,0 +1,109 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollLockButton = document.getElementById("scroll-lock-button");
+  var scrollDownButton = document.getElementById("scroll-down-button");
+  var leftEye = document.getElementById("left-eye");
+  var rightEye = document.getElementById("right-eye");
+  var judgment = document.getElementById("judgment");
+  var isScrollLocked = true;
+
+  // Disable scroll by default
+  if (scrollY !== 0) {
+    document.body.style.overflow = "auto";
+    scrollDownButton.style.display = "none";
+    scrollLockButton.style.display = "none";
+  } else {
+    document.body.style.overflow = "hidden";
+    scrollDownButton.style.display = "none";
+  }
+  // initialize with black screen
+  document.body.style.backgroundColor = "black";
+  judgment.style.display = "none";
+
+  scrollLockButton.addEventListener("click", function () {
+    isScrollLocked = !isScrollLocked;
+
+    if (isScrollLocked) {
+      scrollDownButton.style.display = "none";
+      document.body.style.overflow = "hidden";
+    } else {
+      judgment.style.display = "block";
+      scrollDownButton.style.display = "block";
+      scrollLockButton.style.display = "none";
+      document.body.style.overflow = "auto";
+      document.body.style.backgroundColor = "white";
+      leftEye.style.backgroundColor = "white";
+      rightEye.style.backgroundColor = "white";
+    }
+  });
+
+  scrollDownButton.addEventListener("click", function () {
+    var pageTwo = document.getElementById("page-two");
+    pageTwo.scrollIntoView({ behavior: "smooth" });
+
+    scrollLockButton.style.display = "none";
+    scrollDownButton.style.display = "block";
+    document.body.style.overflow = "auto";
+    isScrollLocked = false;
+  });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      scrollDownButton.style.display = "none";
+    } else {
+      scrollDownButton.style.display = "block";
+    }
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var scrollLockButton = document.getElementById("scroll-lock-button");
+//   var scrollDownButton = document.getElementById("scroll-down-button");
+//   var leftEye = document.getElementById("left-eye");
+//   var rightEye = document.getElementById("right-eye");
+//   var judgment = document.getElementById("judgment");
+//   var isScrollLocked = true;
+
+//   // Disable scroll by default
+//   if (scrollY !== 0) {
+//     document.body.style.overflow = "auto";
+//     scrollDownButton.style.display = "none";
+//     scrollLockButton.style.display = "none";
+//   } else {
+//     document.body.style.overflow = "hidden";
+//     scrollDownButton.style.display = "none";
+//   }
+//   // initialize with black screen
+//   document.body.style.backgroundColor = "black";
+//   judgment.style.display = "none";
+
+//   scrollLockButton.addEventListener("click", function () {
+//     isScrollLocked = !isScrollLocked;
+
+//     if (isScrollLocked) {
+//       scrollDownButton.style.display = "none";
+//       document.body.style.overflow = "hidden";
+//       // Hide eyes with animation
+//       leftEye.style.animation = "hideEyes 4s forwards";
+//       rightEye.style.animation = "hideEyes 4s forwards";
+//     } else {
+//       judgment.style.display = "block";
+//       scrollDownButton.style.display = "block";
+//       scrollLockButton.style.display = "none";
+//       document.body.style.overflow = "auto";
+//       document.body.style.backgroundColor = "white";
+//       leftEye.style.backgroundColor = "white";
+//       rightEye.style.backgroundColor = "white";
+//       // Show eyes with animation
+//       leftEye.style.animation = "revealEyes 4s forwards";
+//       rightEye.style.animation = "revealEyes 4s forwards";
+//     }
+//     window.addEventListener("scroll", function () {
+//       if (window.scrollY > 0) {
+//         scrollDownButton.style.display = "none";
+//       } else {
+//         scrollDownButton.style.display = "block";
+//       }
+//     });
+//   });
+
+//   // ...
+// });
